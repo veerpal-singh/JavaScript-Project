54a169ed96625f2d4615ea72c243f09f2cc78c5a
veerpal-singh <veer.veeru68@gmail.com>
2024-09-29 06:43:35 +0100

    add project five file

---
+++ 05_project/script.js
@@ -0,0 +1,45 @@
+// generate a random color
+
+const randomColor = function () {
+
+    const hex = "0123456789ABCDEF";
+
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+
+    return color;
+}
+
+let intervalId;
+
+const startChangingColor = function () {
+
+    intervalId = setInterval(() => {
+
+        document.body.style.backgroundColor = randomColor()
+
+    }, 1000)
+};
+
+const stopChangingColor = function () {
+
+    clearInterval(intervalId);
+
+    intervalId = null;
+};
+
+document.querySelector("#start").addEventListener
+    ("click", startChangingColor)
+
+document.querySelector("#stop").addEventListener
+    ("click", stopChangingColor)
+
+
+
+
+
+
